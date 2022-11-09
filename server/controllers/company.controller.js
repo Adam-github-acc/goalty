1d38bbed4c1a1260c52452c66c06a7c27212c6d3
Eric Lorite <ericlorpal@gmail.com>
2022-11-09 14:53:54 +0100

    CRUD controller for company

---
+++ server/controllers/company.controller.js
@@ -0,0 +1,103 @@
+const statusCodes = require('../utils/server').status;
+const model = require('../models/db/company');
+const { initialResponse } = require('../utils/server');
+
+
+module.exports = {
+  retrieveAll: async (req, res) => {
+    const response = {...initialResponse};
+    try {
+      const modelResponse = await model.retrieveAll(req.query);
+      if (modelResponse.status) {
+        response.status = statusCodes.ok;
+        response.message = 'Companies found!';
+        response.data = modelResponse.data;
+      } else {
+        response.status = statusCodes.notFound;
+        response.message = 'No companies found.'
+      }
+    } catch (err) {
+      console.log('ERROR-CompanyController-retrieveAll: ', err);
+      response.error = err;
+    }
+
+    res.status(response.status).send(response);
+  },
+
+  retrieveOne: async (req, res) => {
+    const response = { ...initialResponse };
+    try {
+      const modelResponse = await model.retrieveOne(Number(req.params.id));
+
+      if (modelResponse.status && modelResponse.data) {
+        response.status = statusCodes.ok;
+        response.message = 'Company found!';
+        response.data = modelResponse.data;
+      } else {
+        response.status = statusCodes.notFound;
+        response.message = 'Company not found.';
+      }
+
+    } catch (err) {
+      console.log('ERROR-CompanyController-retrieveOne: ', err);
+      response.error = err;
+    }
+    res.status(response.status).send(response);
+  },
+
+  create: async (req, res) => {
+    const response = { ...initialResponse };
+    try {
+      const modelResponse = await model.create(req.body);
+      if (modelResponse) {
+        response.status = statusCodes.created;
+        response.message = 'Company created successfully!';
+        response.data = modelResponse.data;
+      }
+    } catch (err) {
+      console.log('ERROR-CompanyController-create: ', err);
+    }
+
+    res.status(response.status).send(response);
+  },
+
+  update: async (req, res) => {
+    const response = {...initialResponse};
+    try {
+      const modelResponse = await model.update(Number(req.params.id), req.body);
+
+      if (modelResponse && modelResponse.data) {
+        response.status = statusCodes.ok;
+        response.message = 'Company updated successfully!';
+        response.data = modelResponse.data;
+      } else {
+        response.status = statusCodes.notFound;
+        response.message = 'Company not found.';
+      }
+    } catch (err) {
+      console.log('ERROR-CompanyController-update: ', err);
+    }
+
+    res.status(response.status).send(response);
+  },
+
+  delete: async (req, res) => {
+    const response = { ...initialResponse };
+    try {
+      const modelResponse = await model.delete(Number(req.params.id));
+
+      if (modelResponse.status && modelResponse.data) {
+        response.status = statusCodes.noContent;
+        response.message = 'Company deleted successfully!';
+      } else {
+        response.status = statusCodes.notFound;
+        response.message = 'Company not found';
+      }
+    } catch (err) {
+      console.log('ERROR-CompanyController-delete: ', err);
+    }
+
+    res.status(response.status).send(response);
+  }
+
+}
